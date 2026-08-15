b25b3d69e9cacd39b0cebd44408904d3a6d30b0f
이석현 <jrkenniya@gmail.com>
2026-08-15 18:44:15 +0900

    remove: 임의 생성한 mock-data 파일 일괄 삭제

--- mayday_fe/src/api/home-mock-data.js
+++
@@ -1,9 +0,0 @@
-export const HOME_MOCK_DATA = {
-  yearlyExpense: "1,284,000", // 올해 기록한 비용
-  aiFoundExpense: "512,000", // 이번 달 AI가 512,000원이 기록됐어요
-  recordedIncomeRatio: "39", // [올해 수입과 비용]에서 수입의 비율
-  recordedExpenseRatio: "61", // [올해 수입과 비용]에서 비용의 비율
-  recordedIncome: "11,240,000", // 기록한 수입 (11,240,000원)
-  recordedExpense: "31,000,000", // 기록한 비용 (31,000,000원)
-  aiClassifiedRecords: "24", // 이번 달 AI가 분류한 기록 (24건)
-};
