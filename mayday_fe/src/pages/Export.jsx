f3f5892acebab8ed9a06740bd2e37d24946d8e0b
이석현 <jrkenniya@gmail.com>
2026-08-18 18:37:31 +0900

    design: 내보내기, 미리보기 모달 - UI 디자인 수정

--- mayday_fe/src/pages/Export.jsx
+++ mayday_fe/src/pages/Export.jsx
@@ -452,9 +452,6 @@ function Export() {
             </div>
             <span className={styles.arrowIcon} />
           </button>
-          <p className={styles.noticeText}>
-            체크한 항목이 열로 반영돼요 · 좌우로 밀어 전체 열 확인
-          </p>
         </section>
 
         {/* 파일 형식 섹션 */}
